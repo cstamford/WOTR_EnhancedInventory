abe61cc778cc1e8e6d036a1488bcfc8beccc2998
cstamford <cstamford@gmail.com>
2021-09-23 19:39:52 +0100

    Refactor: replace exclusive hook with a transpiler.

--- EnhancedInventory/src/Hooks/ItemsFilterPCView.cs
+++ EnhancedInventory/src/Hooks/ItemsFilterPCView.cs
@@ -3,39 +3,53 @@ using HarmonyLib;
 using Kingmaker.Blueprints.Root;
 using Kingmaker.UI.Common;
 using Kingmaker.UI.MVVM._PCView.Slots;
-using Owlcat.Runtime.UniRx;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
 using UniRx;
 
 namespace EnhancedInventory.Hooks
 {
-    // TODO docs
+    // Handles both adding selected sorters to the sorter dropdowns and making sure that the dropdown is properly updates to match the selected sorter.
     [HarmonyPatch(typeof(ItemsFilterPCView))]
-    public static class ItemsFilterPCView_Initialize
+    public static class ItemsFilterPCView_
     {
-        [HarmonyPrefix]
-        [HarmonyPatch(nameof(ItemsFilterPCView.BindViewImplementation))]
-        public static bool BindViewImplementation(ItemsFilterPCView __instance)
+        private static MethodInfo m_set_dropdown = AccessTools.Method(typeof(ItemsFilterPCView_), nameof(SetDropdown));
+        private static MethodInfo m_set_sorter = AccessTools.Method(typeof(ItemsFilterPCView_), nameof(SetSorter));
+
+        private static void SetDropdown(ItemsFilterPCView instance, ItemsFilter.SorterType val)
         {
-            // TODO: Transpiler? Reverse patch? I don't like this...
-            // It would be nice if we could easily patch the delegates.
-
-            __instance.Show();
-            __instance.SubscribeToggles();
-            __instance.AddDisposable(__instance.ViewModel.CurrentSorter.Subscribe(delegate (ItemsFilter.SorterType value)
-            {
-                __instance.m_Sorter.value = Main.SorterMapper.From((int)value);
-            }));
-            __instance.AddDisposable(__instance.m_Sorter.OnValueChangedAsObservable().Subscribe(delegate (int value)
-            {
-                __instance.ViewModel.SetCurrentSorter((ItemsFilter.SorterType)Main.SorterMapper.To(value));
-            }));
-            __instance.SetTooltips();
-
-            return false;
+            instance.m_Sorter.value = Main.SorterMapper.From((int)val);
         }
 
+        private static void SetSorter(ItemsFilterPCView instance, int val)
+        {
+            instance.ViewModel.SetCurrentSorter((ItemsFilter.SorterType)Main.SorterMapper.To(val));
+        }
+
+        // In BindViewImplementation, there are two inline delegates; we replace both of those in order with our own.
+        [HarmonyTranspiler]
+        [HarmonyPatch(nameof(ItemsFilterPCView.BindViewImplementation))]
+        public static IEnumerable<CodeInstruction> BindViewImplementation(IEnumerable<CodeInstruction> instructions)
+        {
+            List<CodeInstruction> il = instructions.ToList();
+
+            int ldftn_count = 0;
+
+            for (int i = 0; i < il.Count; ++i)
+            {
+                if (il[i].opcode == OpCodes.Ldftn)
+                {
+                    il[i].operand = ldftn_count++ == 0 ? m_set_dropdown : m_set_sorter;
+                }
+            }
+
+            return il.AsEnumerable();
+        }
+
+        // Adds the sorters to the dropdown.
         [HarmonyPostfix]
         [HarmonyPatch(nameof(ItemsFilterPCView.Initialize))]
         public static void Initialize(ItemsFilterPCView __instance)
